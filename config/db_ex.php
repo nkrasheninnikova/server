bdf764794c53d8d1cdf6d9bd7b75079a5eba85c4
Natasha Krasheninnikova <nk110905@mail.ru>
2025-04-10 20:54:48 +0700

    copy db_name commit

---
+++ config/db_ex.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'db_name', //убрать заглушку
+    'username' => 'root', //убрать заглушку
+    'password' => '', //убрать заглушку
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
